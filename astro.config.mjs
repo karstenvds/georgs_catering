aa96c654e231b77aa0c66934ea8a716987b751d0
Karsten von der Sahle <karsten.vondersahle@aoe.com>
2026-05-15 13:06:36 +0200

    chore: add react, tailwind and shadcn
    for easy, quick development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://karstenvds.github.io',
   base: '/georgs_catering',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 })
